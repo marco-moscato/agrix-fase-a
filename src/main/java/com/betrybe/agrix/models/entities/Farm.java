3acb48776d5919513c5ee884f55ed5f6039fe775
Marco Moscato <marcomoscatto@gmail.com>
2023-10-16 14:59:44 -0300

    refactor<Farm>: aplica notations em Farm

--- src/main/java/com/betrybe/agrix/models/entities/Farm.java
+++ src/main/java/com/betrybe/agrix/models/entities/Farm.java
@@ -1,9 +1,20 @@
 package com.betrybe.agrix.models.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
  * Classe Farm representa uma fazenda.
  */
+@Entity
+@Table(name = "farm")
 public class Farm {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String name;
   private Double size;
