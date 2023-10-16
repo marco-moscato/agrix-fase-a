cce40f6e1a10a17474d88c77e24cd9d64d837b41
Marco Moscato <marcomoscatto@gmail.com>
2023-10-16 16:11:41 -0300

    feat<FarmController>: cria rota POST /farms

--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -1,6 +1,13 @@
 package com.betrybe.agrix.controllers;
 
+import com.betrybe.agrix.controllers.dto.FarmDto;
+import com.betrybe.agrix.controllers.dto.ResponseDto;
+import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.services.FarmService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +21,18 @@ public class FarmController {
 
   public FarmController(FarmService farmService) {
     this.farmService = farmService;
+  }
+
+  /**
+   * Método createFarm cria uma nova fazenda.
+   *
+   * @param farmDto uma fazenda no formato Dto
+   * @return status 201 e a fazenda criada com id, name e size
+   */
+  @PostMapping()
+  public ResponseEntity<Farm> createFarm(@RequestBody FarmDto farmDto) {
+    Farm newFarm = farmService.insertFarm(farmDto.toFarm());
+    ResponseDto<Farm> responseDto = new ResponseDto<>("Fazenda criada com suecesso!", newFarm);
+    return ResponseEntity.status(HttpStatus.CREATED).body(responseDto.data());
   }
 }
