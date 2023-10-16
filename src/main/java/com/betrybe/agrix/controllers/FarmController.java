415353eac57887230e5519a21ccae52bdb86eeb6
Marco Moscato <marcomoscatto@gmail.com>
2023-10-16 17:03:25 -0300

    feat: cria rota GET /farms/id em FarmController e Service com Dto

--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -5,10 +5,12 @@ import com.betrybe.agrix.controllers.dto.ResponseDto;
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.services.FarmService;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +43,18 @@ public class FarmController {
   @GetMapping
   public List<Farm> getAllFarms() {
     return farmService.getAllFarms();
+  }
+
+  @GetMapping("/{farmId}")
+  public ResponseEntity<ResponseDto<Farm>> getFarmById(@PathVariable Long farmId) {
+    Optional<Farm> optionalFarm = farmService.getFarmById((farmId));
+
+    if (optionalFarm.isEmpty()) {
+      ResponseDto<Farm> responseDto = new ResponseDto<>("Fazenda não encontrada!", null);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
+    }
+
+    ResponseDto<Farm> responseDto = new ResponseDto<>("Fazenda encontrada com sucesso!", optionalFarm.get());
+    return ResponseEntity.ok(responseDto);
   }
 }
