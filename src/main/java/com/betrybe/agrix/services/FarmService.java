b038d537892764cffefaebd76026e726520e5ec6
Marco Moscato <marcomoscatto@gmail.com>
2023-10-16 15:24:39 -0300

    feat<FarmService>: cria classe com método insertFarm

---
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -0,0 +1,23 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.models.repositories.FarmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * classe service da fazenda.
+ */
+@Service
+public class FarmService {
+  private final FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  public Farm insertFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+}
