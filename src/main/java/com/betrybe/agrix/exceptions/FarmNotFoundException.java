a6745624ab3a2d6a05be9e388a60c48713a4ef9c
Marco Moscato <marcomoscatto@gmail.com>
2023-10-17 15:28:28 -0300

    refactor<FarmNotFoundException>: refatora classe extendendo de Exception conforme aula ao vivo

--- src/main/java/com/betrybe/agrix/exceptions/FarmNotFoundException.java
+++ src/main/java/com/betrybe/agrix/exceptions/FarmNotFoundException.java
@@ -3,7 +3,7 @@ package com.betrybe.agrix.exceptions;
 /**
  * Classe de exceção para fazenda não encontrada.
  */
-public class FarmNotFoundException extends RuntimeException {
+public class FarmNotFoundException extends Exception {
 
   public FarmNotFoundException() {
     super("Fazenda não encontrada!");
